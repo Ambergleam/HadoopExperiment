57a2351bdd8097efefa23194906870e6286eca2a
Ambergleam <ambergleam@gmail.com>
2013-05-16 06:59:54 -0400

    Testing

---
+++ scripts/wordcountimproved.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Hadoop ease-of-use script for an improved word count on Hadoop
+
+# Where the output will go
+OUTPUT_PATH=$"../output"
+
+# Where the input is
+INPUT_PATH=$"../input/input-small"
+
+# Where the jar file is
+JAR_PATH=$"../HadoopExperiment/dist/HadoopExperiment.jar"
+
+# What package inside the jar is being used
+PACKAGE_PATH=$"wordcountimproved.WordCountImproved"
+
+# Remove any previous output files
+rm -rf $OUTPUT_PATH
+
+# Run Hadoop using the various parameters
+hadoop jar $JAR_PATH $PACKAGE_PATH $INPUT_PATH $OUTPUT_PATH
